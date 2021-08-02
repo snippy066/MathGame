df58e1ff79abe6278726eec75c7596eaa95bddb1
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-02 23:18:22 +0530

    Update MainActivity.java

--- app/src/main/java/com/snippy/mathgame/MainActivity.java
+++ app/src/main/java/com/snippy/mathgame/MainActivity.java
@@ -3,8 +3,13 @@ package com.snippy.mathgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button add;
+    Button sub;
+    Button mul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
