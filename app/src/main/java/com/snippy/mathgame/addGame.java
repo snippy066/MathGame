a03eb7fa7beb17149cf0b24304a8f3ea70d7d67a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-02 23:20:03 +0530

    Create addGame.java

---
+++ app/src/main/java/com/snippy/mathgame/addGame.java
@@ -0,0 +1,14 @@
+package com.snippy.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_game);
+    }
+}
