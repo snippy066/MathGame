8b79307352d98c5175ec4938480022261845139c
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-10 12:49:30 +0530

    intializing all variables

--- app/src/main/java/com/snippy/mathgame/addGame.java
+++ app/src/main/java/com/snippy/mathgame/addGame.java
@@ -9,12 +9,20 @@ import android.widget.TextView;
 
 public class addGame extends AppCompatActivity {
 
-    TextView score,life,time,textque;
-    EditText textAns;
-    Button btnOk,btnNext;
+    TextView score,life,time,que;
+    EditText ans;
+    Button ok,next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game);
+
+        score=findViewById(R.id.textSc);
+        life=findViewById(R.id.textLi);
+        time=findViewById(R.id.txtTime);
+        que=findViewById(R.id.textque);
+        ans=findViewById(R.id.textAns);
+        ok=findViewById(R.id.btnOk);
+        next=findViewById(R.id.btnNext);
     }
 }
