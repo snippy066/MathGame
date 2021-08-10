522fed135392ee54f77cf130bd158f7205dfd6ae
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-10 13:07:43 +0530

    Update addGame.java

--- app/src/main/java/com/snippy/mathgame/addGame.java
+++ app/src/main/java/com/snippy/mathgame/addGame.java
@@ -3,15 +3,22 @@ package com.snippy.mathgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class addGame extends AppCompatActivity {
 
     TextView score,life,time,que;
     EditText ans;
     Button ok,next;
+
+    Random rnd=new Random();
+    int num1,num2,correct_ans;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +31,49 @@ public class addGame extends AppCompatActivity {
         ans=findViewById(R.id.textAns);
         ok=findViewById(R.id.btnOk);
         next=findViewById(R.id.btnNext);
+
+        //ok button listener
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int userans=Integer.parseInt(ans.getText().toString());
+
+                if(userans==correct_ans){
+                    int count=Integer.parseInt(score.getText().toString());
+                    count+=10;
+
+                    score.setText(count);
+
+                    que.setText("Congratulations your answer is true");
+                }
+                else{
+                    int usrli=Integer.parseInt(life.getText().toString());
+                    usrli--;
+                    
+                }
+
+            }
+        });
+
+        //next button listener
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+    //game logic
+    public void gameStart(){
+        num1=rnd.nextInt(100);
+        num2=rnd.nextInt(100);
+
+        correct_ans=num1+num2;
+
+        que.setText(num1+" + "+num2);
+
     }
 }
