0f9753f4a884ed9ddf47536b58c80506b2a762dd
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-10 13:12:41 +0530

    added next button

--- app/src/main/java/com/snippy/mathgame/addGame.java
+++ app/src/main/java/com/snippy/mathgame/addGame.java
@@ -64,7 +64,7 @@ public class addGame extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                gameStart();
             }
         });
 
