a263e5bf691834e6f538671a5e16371d97e26c00
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-10 12:40:49 +0530

    Update addGame.java

--- app/src/main/java/com/snippy/mathgame/addGame.java
+++ app/src/main/java/com/snippy/mathgame/addGame.java
@@ -3,9 +3,15 @@ package com.snippy.mathgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class addGame extends AppCompatActivity {
 
+    TextView score,life,time,textque;
+    EditText textAns;
+    Button btnOk,btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
